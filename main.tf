fc2e69f64595ff5afc8b9e2b04710bb5a67e064f
lokesh7devops <115445012+lokesh7devops@users.noreply.github.com>
2022-10-12 15:15:22 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,7 @@
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
 
+resource "azurerm_storage_account" "storage_account" { 
+  name = var.name
