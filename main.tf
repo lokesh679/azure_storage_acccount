6283f11584166534e920b6ebae66c81b5be62e09
lokesh7devops <115445012+lokesh7devops@users.noreply.github.com>
2022-10-12 15:17:49 +0530

    formatting code

--- main.tf
+++ main.tf
@@ -4,4 +4,9 @@ resource "azurerm_resource_group" "rg" {
 }
 
 resource "azurerm_storage_account" "storage_account" { 
-  name = var.name
+  name                = var.storage_name
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  account_tier        = var.account_tier
+  replication_type    = var.replication_type
+}
